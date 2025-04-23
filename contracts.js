58a3507924f473a99dc50e81826fd3086faa52b9
Mohit-2219 <mohit0458.becse24@chitkara.edu.in>
2025-04-23 14:13:57 +0530

    Create contracts.js

---
+++ contracts.js
@@ -0,0 +1,49 @@
+// Smart Contract Integration for Tixify
+// This file handles all interactions with the blockchain contracts
+
+// Contract Addresses
+const CONTRACT_ADDRESSES = {
+    upgradeability: "0x0cE2475b04dE1BF2fbc3DE267F731707b766fd35",
+    ticketTypes: "0x66Bbe67230d35142f85C7D932c7457ee448969e0",
+    ticketMarketplace: "0xeEF8487CD2225C5C6f4a7DE175d9A60281121806",
+    access: "0xd48144679D25fBAaecDCA1Ef81a37EBC75dcEc92",
+    groupDiscounts: "0xAee0E1f6dD19A03066C7c5C10cf098Bc7b8437DB"
+};
+
+// ABIs (Application Binary Interfaces)
+// These will need to be replaced with your actual contract ABIs
+const TICKET_TYPES_ABI = [
+    // Example ABI for TicketTypes contract
+    {
+        "inputs": [{"internalType": "string", "name": "eventName", "type": "string"}],
+        "name": "createTicketType",
+        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "uint256", "name": "typeId", "type": "uint256"}],
+        "name": "getTicketType",
+        "outputs": [
+            {"internalType": "string", "name": "eventName", "type": "string"},
+            {"internalType": "uint256", "name": "price", "type": "uint256"},
+            {"internalType": "uint256", "name": "supply", "type": "uint256"}
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+
+const TICKET_MARKETPLACE_ABI = [
+    // Example ABI for TicketMarketplace contract
+    {
+        "inputs": [
+            {"internalType": "uint256", "name": "typeId", "type": "uint256"},
+            {"internalType": "uint256", "name": "quantity", "type": "uint256"}
+        ],
+        "name": "purchaseTickets",
+        "outputs": [{"internalType": "uint256[]", "name": "", "type": "uint256[]"}],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
